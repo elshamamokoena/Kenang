8581fa880af22e16d50b44be221a91c4fa8709c5
Elshama Mokoena <elshamamokoena@gmail.com>
2023-03-13 16:31:43 +0200

    Initial Commit

---
+++ Kenang/Controllers/ServiceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kenang.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WebDevelopment()
+        {
+            return View();
+        }
+    }
+}
---
+++ Kenang/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kenang.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
